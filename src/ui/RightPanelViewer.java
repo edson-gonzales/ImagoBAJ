bd77213c75b468ca1b352592fdfa7df43512293c
AlvaroDaza <Alvaro.Daza@fundacion-jala.org>
2016-07-13 12:59:32 -0400

    first iteration

---
+++ src/ui/RightPanelViewer.java
@@ -0,0 +1,27 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Administrator on 7/5/2016.
+ */
+public class RightPanelViewer extends JPanel {
+
+    private JPanel leftPanelViewer;
+    public RightPanelViewer(String title)
+    {
+
+        setBackground(Color.RED);
+
+    }
+    public void initCOmponents()
+    {
+        leftPanelViewer= new JPanel();
+    }
+    public void addComponents()
+    {
+
+    }
+
+}
